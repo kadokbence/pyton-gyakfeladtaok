9aa9202844eedd586862aeeb2e082c485e82a39a
kadokbence <162699637+kadokbence@users.noreply.github.com>
2025-04-02 12:05:47 +0200

    Add files via upload

---
+++ osszegzes.py
@@ -0,0 +1,27 @@
+t = [3,8,2,4,5,1,6]
+oszzeg = 0
+for i in range(2,len(t)):
+    oszzeg += t[i]
+print("öszzeg:",oszzeg) 
+#i: ciklusszámláló
+
+#tömb: ennek az elemeit fogjuk összeadni
+
+#összeg: összeadások eredménye
+
+#Algoritmus:
+
+#i:=0
+
+#összeg:=0
+
+#ciklus 1-től a tömb végéig, egyesével növekedve
+
+#    ha a feltétel igaz, akkor összeg:=összeg+tömb[i]
+
+#     elágazás vége
+
+#ciklus vége
+
+#ki: összeg
+
